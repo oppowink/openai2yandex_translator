bab3314756c969005681eab26130553d3ae64dd5
Полинка <traktorletit@gmail.com>
2026-03-01 23:15:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ import os, sys, json
 from loguru import logger
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
-
 load_dotenv()
-
 # Загрузка конфига
 with open('config.json', 'r') as f:
     config = json.load(f)
-
 # Уровень логирования
 LOG_LEVEL = config.get("log_level", "INFO")
-
 # Проверяем, запущено ли приложение на Vercel
 is_vercel = os.getenv("VERCEL", False)
-
 # Настраиваем логирование
 if is_vercel:
     # Логи выводятся в консоль
@@ -24,18 +19,13 @@ if is_vercel:
 else:
     # Логи записываются в файл
     logger.add("logs/debug.log", format="{time} {level} {message}", level=LOG_LEVEL, rotation="100 MB")
-
 main_app = FastAPI(logger=logger)
-app.add_middleware(
+main_app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 main_app.include_router(app)
-
 if __name__ == "__main__":
     import uvicorn
-    
-    uvicorn.run("main:main_app", host="0.0.0.0", port=9041, reload=True, log_level="info")
